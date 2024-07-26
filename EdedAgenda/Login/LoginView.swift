0d203e9e68f4d961382a7f240a64d0fe4ed099cb
Eliana Bohorquez <Eliana15030208@gmail.com>
2024-07-26 15:08:05 -0500

    configure of view login

--- EdedAgenda/Login/LoginView.swift
+++ EdedAgenda/Login/LoginView.swift
@@ -10,65 +10,69 @@ import SwiftUI
 struct LoginView: View {
     
     var body: some View {
-        ZStack {
-            Color.backgroundColor.ignoresSafeArea(.all)
-            
-            VStack {
-                Text(LoginText.welcome)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .fontDesign(.rounded)
-                    .padding(.top, 150)
-                TextField(LoginText.username, text: .constant(""))
-                    .padding(.horizontal, 60)
-                    .padding(.top, 100)
+        NavigationView(content: {
+            ZStack {
+                Color.backgroundColor.ignoresSafeArea(.all)
                 
-                Divider()
-                    .padding(.horizontal, 50)
-                TextField(LoginText.password, text: .constant(""))
-                    .padding(.horizontal, 60)
+                VStack {
+                    Text(LoginText.welcome)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .fontDesign(.rounded)
+                        .padding(.top, 150)
+                    TextField(LoginText.username, text: .constant(""))
+                        .padding(.horizontal, 60)
+                        .padding(.top, 100)
+                    
+                    Divider()
+                        .padding(.horizontal, 50)
+                    TextField(LoginText.password, text: .constant(""))
+                        .padding(.horizontal, 60)
+                        .padding(.top)
+                        
+                    Divider()
+                        .padding(.horizontal, 50)
+                    
+                    NavigationLink(destination: Text("Ingresar")) {
+                        Text(LoginText.login)
+                    }
+                    .padding()
+                    .background(Color.buttonLoginColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 17))
+                    .padding(.top, 20)
+                    .font(.callout)
+                    
+
+                    Text(LoginText.forgotPassword)
+                        .padding(.horizontal, 90)
+                        .padding(.top, 5)
+                        .font(.caption)
+                    
+                    NavigationLink(destination: Text("Registrarse")) {
+                        Text(LoginText.register)
+                        
+                    }
+                    .padding()
+                    .background(Color.buttonLoginColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 17))
                     .padding(.top)
+                    .font(.callout)
                     
-                Divider()
-                    .padding(.horizontal, 50)
-                
-                Button(LoginText.login) {
-        
-                }
-                .padding()
-                .background(Color.buttonLoginColor)
-                .clipShape(RoundedRectangle(cornerRadius: 17))
-                .padding(.top, 20)
-                .font(.callout)
-                
-                Text(LoginText.forgotPassword)
-                    .padding(.horizontal, 90)
-                    .padding(.top, 5)
-                    .font(.caption)
-                
-                Button(LoginText.register) {
+                    Text(LoginText.donNotHaveAnAccount)
+                        .padding(.horizontal, 110)
+                        .padding(.top, 5)
+                        .font(.caption)
                     
+                    Text(LoginText.startSectionWith)
+                        .padding(.horizontal, 120)
+                        .padding(.top, 10)
+                    
+                    
+                    
+                 Spacer()
                 }
-                .padding()
-                .background(Color.buttonLoginColor)
-                .clipShape(RoundedRectangle(cornerRadius: 17))
-                .padding(.top)
-                .font(.callout)
-                
-                Text(LoginText.donNotHaveAnAccount)
-                    .padding(.horizontal, 110)
-                    .padding(.top, 5)
-                    .font(.caption)
-                
-                Text(LoginText.startSectionWith)
-                    .padding(.horizontal, 120)
-                    .padding(.top, 10)
-                
-                
-                
-             Spacer()
             }
-        }
+        })
         
     }
 }
